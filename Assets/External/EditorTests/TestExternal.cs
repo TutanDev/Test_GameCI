7a1a3631265be066658ba58c184361218365f484
tutan09 <andrespino.95@gmail.com>
2023-09-28 10:37:04 +0200

    Initial commit

---
+++ Assets/External/EditorTests/TestExternal.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Nostraverse.PlayModeTests
+{
+	public class TestExternal
+	{
+		[Test]
+		public void TestNostraverseSimplePasses()
+		{
+			Assert.IsTrue(false);
+		}
+	}
+}
---
+++ Assets/Nostraverse/EditorTests/TestNostraverse.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Nostraverse.PlayModeTests
+{
+	public class TestNostraverse
+	{
+		[Test]
+		public void TestNostraverseSimplePasses()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
